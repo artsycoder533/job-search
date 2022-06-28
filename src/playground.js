29ce3569d5a2210376d11ab5db6a037cb5c7f6ad
Natasha Johnson <njohnson533@gmail.com>
2022-06-28 14:18:40 -0400

    styled header component

--- src/playground.js
+++ src/playground.js
@@ -2,3 +2,21 @@ const fruits = ["Apple", "Banana", "Orange"];
 const vegetables = ["Cucumber", "Radish"];
 
 console.log([...fruits, ...vegetables]);
+
+const developer = {
+  salary: 100000,
+  lookingForWork: true,
+  techStack: ["Vue", "Next", "React"],
+  doubleSalary() {
+    this.salary = this.salary * 2;
+    this.lookingForWork = false;
+  }
+}
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
+
+developer.doubleSalary();
+
+console.log(developer.salary);
+console.log(developer.lookingForWork);
