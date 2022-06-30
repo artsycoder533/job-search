41a5007fa574d9a8a6a13a60662e5e1e9ca0556b
Natasha Johnson <njohnson533@gmail.com>
2022-06-29 22:48:41 -0400

    added v-for for menu items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,14 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1">
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <a :href="url" class="flex items-center h-full text-xl">{{ company }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="h-full ml-9 first:ml-0"> 
+              <a href="" class="flex items-center h-full py-2.5 ">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -14,7 +21,8 @@ export default {
   data() {
     return {
       company: "Natasha's Careers",
-      url: "https://careers.google.com"
+      url: "https://careers.google.com",
+      menuItems: ["Teams", "Locations", "Life at Natasha", "How We Hire", "Students", "Jobs"],
     };
   },
 };
