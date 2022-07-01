fd48c6e60f274f553847edaf3a292c52d6df06da
Natasha Johnson <njohnson533@gmail.com>
2022-07-01 14:18:06 -0400

    added test for ActionButton component

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,26 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton";
+
+describe("ActionButton", () => {
+  it("renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I'm so clickable",
+        type: "primary",
+      },
+    });
+    // check whether the wrapper buttons text contains the substring
+    expect(wrapper.text()).toMatch("I'm so clickable");
+  });
+
+  it("applies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  })
+});
