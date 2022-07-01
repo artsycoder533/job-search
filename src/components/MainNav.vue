f3a039a764044fa1c9372b41761f9212b0ecdfa4
Natasha Johnson <njohnson533@gmail.com>
2022-07-01 14:28:53 -0400

    add prop validation to ActionButton

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -26,7 +26,6 @@
           <action-button
             v-else
             text="Sign In"
-            type="primary"
             data-test="login-button"
             @click="loginUser"
           />
